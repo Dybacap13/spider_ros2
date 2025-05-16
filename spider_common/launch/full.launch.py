6b619680daa552b2c8ca1667275f0dd5ea78f78e
Anya Bannikova <1q2s3c4g@bk.ru>
2025-05-16 12:49:10 +0300

    чтоб рвиз запустился для показа

--- spider_common/launch/full.launch.py
+++ spider_common/launch/full.launch.py
@@ -102,7 +102,7 @@ def generate_launch_description():
 # rviz
 
   declared_arguments.append(
-        DeclareLaunchArgument("launch_rviz", default_value="false", description="Launch RViz?")
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
     )
 
   
@@ -249,33 +249,35 @@ def generate_launch_description():
         ],
     )
   
-      # Запуск мира Gazebo
+#       # Запуск мира Gazebo
 
-    # Путь к пакету gazebo_ros
-  gazebo_ros_pkg = get_package_share_directory('gazebo_ros')
+#     # Путь к пакету gazebo_ros
+#   gazebo_ros_pkg = get_package_share_directory('gazebo_ros')
     
-    # Запуск Gazebo с пустым миром
-  gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(gazebo_ros_pkg, 'launch', 'gazebo.launch.py')
-        ),
-        launch_arguments={'world': 'empty.world'}.items()
-    )
+#     # Запуск Gazebo с пустым миром
+#   gazebo = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource(
+#             os.path.join(gazebo_ros_pkg, 'launch', 'gazebo.launch.py')
+#         ),
+#         launch_arguments={'world': 'empty.world'}.items()
+#     )
   
   
-        # Загрузка модели робота (например, TurtleBot3)
-  spawn_robot = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'my_robot',  # Имя робота в Gazebo
-            '-topic', 'robot_description',  # Топик, откуда брать URDF/SDF
-            '-x', '0.0',  # Позиция X
-            '-y', '0.0',  # Позиция Y
-            '-z', '0.1'   # Позиция Z (чтобы не упал)
-        ],
-        output='screen'
-    )
+#         # Загрузка модели робота (например, TurtleBot3)
+#   spawn_robot = Node(
+#         package='gazebo_ros',
+#         executable='spawn_entity.py',
+#         arguments=[
+#             '-entity', 'my_robot',  # Имя робота в Gazebo
+#             '-topic', 'robot_description',  # Топик, откуда брать URDF/SDF
+#             '-x', '0.0',  # Позиция X
+#             '-y', '0.0',  # Позиция Y
+#             '-z', '0.1'   # Позиция Z (чтобы не упал)
+#         ],
+#         output='screen'
+#     )
+  
+
   robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -356,8 +358,8 @@ def generate_launch_description():
   control_node_start.append(delay_joint_state_broadcaster_spawner_after_ros2_control_node)
   control_node_start.append(container_parametrs)
   control_node_start.append(container_gazebo)
-  control_node_start.append(spawn_robot)
-  control_node_start.append(gazebo)
+#   control_node_start.append(spawn_robot)
+#   control_node_start.append(gazebo)
   
 
 
--- spider_common/launch/gazebo.launch.py
+++ spider_common/launch/gazebo.launch.py
@@ -1,118 +1,160 @@
-from launch import LaunchDescription
-from launch.event_handlers import OnProcessExit
-from launch.actions import RegisterEventHandler
-from launch_ros.actions import Node
+# from launch import LaunchDescription
+# from launch.event_handlers import OnProcessExit
+# from launch.actions import RegisterEventHandler
+# from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
-from launch.substitutions import Command
+# from launch.actions import IncludeLaunchDescription, ExecuteProcess
+# from launch.substitutions import Command
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# import os
+# from launch.actions import TimerAction
+
+# def generate_launch_description():
+#     # Путь к пакету inspector_gazebo
+#     pkg_inspector_gazebo = get_package_share_directory('spider_description')
+
+#     # Запуск мира Gazebo
+
+#     default_world = os.path.join(
+#         get_package_share_directory('spider_controllers'),
+#         'worlds',
+#         'empty.world'
+#     )
+#     print(default_world)
+
+#     gazebo = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource(
+#             os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+#         ),
+#         launch_arguments={'gz_args': ['-r ', default_world], 'on_exit_shutdown': 'true'}.items()
+#     )
+
+#     # Загрузка модели робота
+#     robot_description = Command(['xacro ', str(os.path.join(pkg_inspector_gazebo, 'urdf', 'spider.urdf.xacro'))])
+    
+#     robot_state_publisher = Node(
+#         package='robot_state_publisher',
+#         executable='robot_state_publisher',
+#         name='robot_state_publisher',
+#         output='screen',
+#         parameters=[{'robot_description': robot_description}]
+#     )
+
+#     # Спавн робота в Gazebo
+#     spawn_entity = Node(
+#         package='gazebo_ros',
+#         executable='spawn_entity.py',
+#         arguments=[
+#             '-topic', 'robot_description',
+#             '-entity', 'spider',
+#             '-x', '0.0',
+#             '-y', '0.0', 
+#             '-z', '1.0',
+            
+#         ],
+#         output='screen'
+#     )
+
+#     # bridge_params = os.path.join(get_package_share_directory('inspector_gazebo'),'config','gz_bridge.yaml')
+#     # ros_gz_bridge = Node(
+#     #     package="ros_gz_bridge",
+#     #     executable="parameter_bridge",
+#     #     arguments=[
+#     #         '--ros-args',
+#     #         '-p',
+#     #         f'config_file:={bridge_params}',
+#     #     ]
+#     # )
+
+#     # ros_gz_image_bridge = Node(
+#     #     package="ros_gz_image",
+#     #     executable="image_bridge",
+#     #     arguments=["/camera/image_raw"]
+#     # )
+
+#     joint_broad_spawner = ExecuteProcess(
+#         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+#              'joint_state_broadcaster'],
+#         output='screen'
+#     )
+
+#     forward_command_controller = ExecuteProcess(
+#         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+#              'forward_command_controller'],
+#         output='screen'
+#     )
+
+#     activate_forward_command_controller = ExecuteProcess(
+#         cmd=['ros2', 'control', 'set_controller_state', 'forward_command_controller', 'active'],
+#         output='screen'
+#     )
+#     spawn_robot_delayed = TimerAction(
+#     period=5.0,  # Ждём 5 секунд
+#     actions=[spawn_entity],
+#     )
+    
+#     return LaunchDescription([
+#         # RegisterEventHandler(
+#         #     event_handler=OnProcessExit(
+#         #         target_action=spawn_entity,
+#         #         on_exit=[forward_command_controller],
+#         #     )
+#         # ),
+
+#         # RegisterEventHandler(
+#         #     event_handler=OnProcessExit(
+#         #         target_action=forward_command_controller,
+#         #         on_exit=[joint_broad_spawner],
+#         #     )
+#         # ),
+
+#         gazebo,
+#         robot_state_publisher,
+#         spawn_robot_delayed,
+#        # ros_gz_bridge,
+#         #activate_forward_command_controller,  # Нужно для активации контроллера из-за 
+#                                               # ошибки ожидания переключения контроллеров более 5 секнунд
+#         # ros_gz_image_bridg
+        
+#     ])
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
-from launch.actions import TimerAction
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
-    # Путь к пакету inspector_gazebo
-    pkg_inspector_gazebo = get_package_share_directory('spider_description')
+    ros_gz_sim_pkg_path = get_package_share_directory('ros_gz_sim')
+    example_pkg_path = FindPackageShare('example_package')  # Replace with your own package name
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
 
-    # Запуск мира Gazebo
-
-    default_world = os.path.join(
-        get_package_share_directory('spider_controllers'),
-        'worlds',
-        'empty.world'
-    )
-
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
-        ),
-        launch_arguments={'gz_args': ['-r ', default_world], 'on_exit_shutdown': 'true'}.items()
-    )
-
-    # Загрузка модели робота
-    robot_description = Command(['xacro ', str(os.path.join(pkg_inspector_gazebo, 'urdf', 'spider.urdf.xacro'))])
-    
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description}]
-    )
-
-    # Спавн робота в Gazebo
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-topic', 'robot_description',
-            '-entity', 'spider',
-            '-x', '0.0',
-            '-y', '0.0', 
-            '-z', '1.0',
-            
-        ],
-        output='screen'
-    )
-
-    # bridge_params = os.path.join(get_package_share_directory('inspector_gazebo'),'config','gz_bridge.yaml')
-    # ros_gz_bridge = Node(
-    #     package="ros_gz_bridge",
-    #     executable="parameter_bridge",
-    #     arguments=[
-    #         '--ros-args',
-    #         '-p',
-    #         f'config_file:={bridge_params}',
-    #     ]
-    # )
-
-    # ros_gz_image_bridge = Node(
-    #     package="ros_gz_image",
-    #     executable="image_bridge",
-    #     arguments=["/camera/image_raw"]
-    # )
-
-    joint_broad_spawner = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    forward_command_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'forward_command_controller'],
-        output='screen'
-    )
-
-    activate_forward_command_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'set_controller_state', 'forward_command_controller', 'active'],
-        output='screen'
-    )
-    spawn_robot_delayed = TimerAction(
-    period=5.0,  # Ждём 5 секунд
-    actions=[spawn_entity],
-    )
-    
     return LaunchDescription([
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=spawn_entity,
-        #         on_exit=[forward_command_controller],
-        #     )
-        # ),
+        SetEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            PathJoinSubstitution([example_pkg_path, 'models'])
+        ),
+        SetEnvironmentVariable(
+            'GZ_SIM_PLUGIN_PATH',
+            PathJoinSubstitution([example_pkg_path, 'plugins'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': [PathJoinSubstitution([example_pkg_path, 'worlds/example_world.sdf'])],  # Replace with your own world file
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
 
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=forward_command_controller,
-        #         on_exit=[joint_broad_spawner],
-        #     )
-        # ),
-
-        gazebo,
-        robot_state_publisher,
-        spawn_robot_delayed,
-       # ros_gz_bridge,
-        #activate_forward_command_controller,  # Нужно для активации контроллера из-за 
-                                              # ошибки ожидания переключения контроллеров более 5 секнунд
-        # ros_gz_image_bridg
-        
+        # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=['/example_imu_topic@sensor_msgs/msg/Imu@gz.msgs.IMU',],
+            remappings=[('/example_imu_topic',
+                         '/remapped_imu_topic'),],
+            output='screen'
+        ),
     ])
